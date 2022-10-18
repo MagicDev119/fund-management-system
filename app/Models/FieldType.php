50adc18b3f73cdc03264483de45b621b0fcfffa5
MagicDev119 <melnykovychadrian@gmail.com>
2022-10-18 14:38:45 +0300

    fix input setting issue

--- app/Models/FieldType.php
+++ app/Models/FieldType.php
@@ -4,11 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FieldType extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
     protected $table = 'field_type';
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'cate_name',
+        'cate_name',
+        'table'
+    ];
 
     public function fundFields() {
         return $this->hasMany(FundField::class, 'field_type_id');
