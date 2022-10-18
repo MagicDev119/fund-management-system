d296b66742342c358026c04232b4408e253ccd0b
MagicDev119 <melnykovychadrian@gmail.com>
2022-10-18 14:29:45 +0300

    fix input setting issue

--- app/Http/Controllers/AssetFieldGroupController.php
+++ app/Http/Controllers/AssetFieldGroupController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 
+use \App\models\AssetFieldGroup;
 use \App\models\FieldType;
 
 class AssetFieldGroupController extends Controller
