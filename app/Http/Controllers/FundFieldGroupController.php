d0aa32f5aca50fba03dbe25d3f3f0b120996badd
MagicDev119 <melnykovychadrian@gmail.com>
2022-10-18 20:12:38 +0300

    fix input setting issue

--- app/Http/Controllers/FundFieldGroupController.php
+++ app/Http/Controllers/FundFieldGroupController.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 
 use \App\models\FundFieldGroup;
-use \App\models\FieldType;
+use \App\models\Fund;
 
 class FundFieldGroupController extends Controller
 {
@@ -33,7 +33,7 @@ class FundFieldGroupController extends Controller
             $value['fieldCnt'] = count($value->fundFields);
             $fieldGroups[] = $value;
         }
-        $fieldType = FieldType::get();
+        $fieldType = Fund::get();
 
         return response()->json([
             'fieldGroups' => $fieldGroups,
